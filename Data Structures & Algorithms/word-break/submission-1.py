60ef0ad25f55116640ca9353470c5dadf993ba89
Ishita <68499445+ishi03@users.noreply.github.com>
2026-06-04 12:50:53 -0400

    Add: word-break - submission-1

---
+++ Data Structures & Algorithms/word-break/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        dp = [False] * (n+1)
+        dp[0] = True # empty string can be made by choosing none of the words
+        for i in range(1, n+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+        return dp[n]
