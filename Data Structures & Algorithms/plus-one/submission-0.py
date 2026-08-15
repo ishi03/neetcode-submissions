519efb0fe6d0b30210c210274ae3334668d59039
Ishita <68499445+ishi03@users.noreply.github.com>
2026-08-15 13:23:32 -0400

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 1
+        for i in range(len(digits)-1, -1, -1):
+            digit = digits[i]
+            digit += carry
+            digits[i] = (digit%10)
+            carry = (digit//10)
+        if carry:
+            digits.insert(0, carry)
+        return digits
